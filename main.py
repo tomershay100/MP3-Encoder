3c80c7f0618e00678015b49428f87bd35ad9d52c
Tomer Shay <tomershay100@gmail.com>
2022-05-11 20:09:57 +0300

    create WAV_Reader.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+from WAV_Reader import WavReader
+
+if __name__ == "__main__":
+    if len(sys.argv) > 2:
+        sys.exit('Unexpected number of arguments.')
+    if len(sys.argv) < 2:
+        sys.exit('No directory specified.')
+    file_path = sys.argv[1]
+
+    if not os.path.exists(file_path):
+        sys.exit('File not found.')
+    # default_mpeg = {"bit_rate": 64, "emp": None, "copyright": 0, "original": 1}
+    wav_file = WavReader(file_path)
+    # encoder = MP3Encoder(wav_file)
+    # encoder.encode()
